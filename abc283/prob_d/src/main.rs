3e01b3601144a935715ed94add4e1f63fbc71b34
ponsaaan <mttkd399@gmail.com>
2022-12-25 17:27:02 +0900

    add: prob d

--- abc283/prob_d/src/main.rs
+++ abc283/prob_d/src/main.rs
@@ -1,4 +1,33 @@
+use std::collections::HashSet;
+
 use proconio::input;
 fn main() {
-    // TODO
+    input! {
+        s: String,
+    }
+
+    let mut st: Vec<HashSet<char>> = Vec::new();
+    let mut set: HashSet<char> = HashSet::new();
+    st.push(HashSet::new());
+    for c in s.chars() {
+        match c {
+            '(' => st.push(HashSet::new()),
+            ')' => {
+                let remove_target = st.pop().unwrap();
+                for target in remove_target {
+                    set.remove(&target);
+                }
+            }
+            _ => {
+                if set.contains(&c) {
+                    println!("No");
+                    return;
+                }
+                st.last_mut().unwrap().insert(c);
+                set.insert(c);
+            }
+        }
+    }
+
+    println!("Yes");
 }
