fd80958e38f515cc35a0880fb35dcf7e1bf48062
ponsaaan <mttkd399@gmail.com>
2023-01-29 18:07:22 +0900

    add: abc286

--- abc286/prob_d/src/main.rs
+++ abc286/prob_d/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::{hash_set, HashSet};
-
 use proconio::input;
 
 fn main() {
@@ -9,27 +7,41 @@ fn main() {
         ab: [(usize,usize);n]
     }
 
-    let mut hash_set = HashSet::new();
-    let first = ab[0];
-    for i in 0..ab[0].1 + 1 {
-        hash_set.insert(first.0 * i);
-    }
-
-    for (i, (a, b)) in ab.iter().enumerate() {
-        if i == 0 {
-            continue;
+    let mut dp = vec![vec![false;x+1];n];
+    let target_a0 = ab[0].0;
+    let target_b0 = ab[0].1;
+    for j in 0..target_a0 * target_b0+1 {
+        if j > x {
+            break;
         }
-
-        for k in hash_set.clone() {
-            for l in 0..b + 1 {
-                hash_set.insert(k + a * l);
+        if j % target_a0 == 0 {
+            dp[0][j] = true
+        }
+    }
+    for i in 1..n {
+        let target_a = ab[i].0;
+        let target_b = ab[i].1;
+        for j in 0..x+1 {
+            if dp[i-1][j] {
+                dp[i][j] = true;
+                continue;
+            }
+            for k in 0..target_b+1 {
+                if j < k*target_a {
+                    continue;
+                }
+                if dp[i-1][j - k*target_a] {
+                    dp[i][j] = true;
+                }
             }
         }
     }
 
-    if hash_set.contains(&x) {
-        println!("Yes")
-    } else {
-        println!("No")
+    for i in 0..n {
+        if dp[i][x] {
+            println!("Yes");
+            return;
+        }
     }
+    println!("No")
 }
